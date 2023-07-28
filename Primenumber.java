c2c44c5fb7b4506b7e30c610e2f385546efcca52
AbinayaRaveendran <140489289+AbinayaRaveendran@users.noreply.github.com>
2023-07-28 13:02:54 +0530

    Add files via upload
    week1day1 assignment

---
+++ Primenumber.java
@@ -0,0 +1,27 @@
+package week1day1;
+
+public class Primenumber {
+
+	public static void main(String[] args) {
+		int n=15;
+		
+		for(int i=2;i<n;i++)
+		{
+			if(n%i==0)
+			{
+	System.out.println("the given number is not a prime");
+			}
+			else
+			{
+				System.out.println("the given number is prim");
+				
+			}
+	
+			
+			
+			
+		}
+
+	}
+
+}
